e733185c95e2106dcdaa03c0ac86333e12dafe81
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2026-07-12 03:58:54 +0900

    데이터베이스 초기화 명령어 수정

--- RaisingPetServer/database/AccountInitialization.sql
+++ RaisingPetServer/database/AccountInitialization.sql
@@ -2,13 +2,21 @@ USE RaisingPet;
 
 SET FOREIGN_KEY_CHECKS = 0;
 
+-- Reset stock-related player state.
 TRUNCATE TABLE StockPrice;
 TRUNCATE TABLE StockTrade;
 TRUNCATE TABLE StockSale;
 TRUNCATE TABLE StockHolding;
 TRUNCATE TABLE Stock;
+
+-- Reset savings/loan subscriptions while keeping product tables intact.
 TRUNCATE TABLE PlayerLoan;
 TRUNCATE TABLE PlayerSavings;
-TRUNCATE TABLE Player;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+-- Keep accounts, but reset runtime/player-owned state.
+UPDATE Player
+SET
+    Money = 0,
+    IsOnline = FALSE;
